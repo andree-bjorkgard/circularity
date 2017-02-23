561f6999e413ed2716275e48713f0d0f32f10036
Andrée Björkgård <andree.bjorkgard@billes.se>
2017-02-23 23:44:22 +0100

    tests for sector

---
+++ src/components/__tests__/Sector.spec.js
@@ -0,0 +1,43 @@
+/*global beforeEach, expect, jest, test */
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Sector from '../Sector'
+
+let props
+
+beforeEach(() => {
+  props = {
+    increment: 40,
+    initialAngle: -90,
+    isHovered: false,
+    item: { action: 'http://google.se', image: '/path/to/img', name: 'whoops' },
+    onHoverChange: () => 33,
+    radius: 200,
+  }
+})
+
+const component = (props) => renderer.create(
+    <Sector {...props} />
+)
+
+test('When hovered over sector, onHoverChange should be called', () => {
+  const mock = jest.fn()
+  props.onHoverChange = mock
+
+  let tree = component(props).toJSON()
+  tree.props.onMouseEnter()
+
+  expect(mock).toHaveBeenCalled()
+})
+
+test('Fill color of sector changes when prop isHovered toggles', () => {
+  props.isHovered = false
+  let tree = component(props).toJSON()
+
+  expect(tree).toMatchSnapshot()
+
+  props.isHovered = true
+  tree = component(props).toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
