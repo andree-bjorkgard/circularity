3e9608a20df4d0bed6a52078da8383c1a741b27f
Andrée Björkgård <andree.bjorkgard@billes.se>
2017-02-23 10:42:54 +0100

    fixing proptypes

--- src/components/Item.js
+++ src/components/Item.js
@@ -29,32 +29,38 @@ const getPieData = (r, initialAngle, inc, centerX = r, centerY = r) => {
   return `M${centerX},${centerY} L${x1},${y1} A${r},${r} 0 0 1 ${x2},${y2} z`
 }
 
-const Item = ({item, radius, initialAngle, increment, actions, centerX = radius, centerY = radius}) => (
+const Item = ({item, radius, initialAngle, increment, actions}) => (
   <a onMouseEnter={() => actions.hovered(item)} onMouseLeave={() => actions.unHovered(item)} className={cxs(s.link)} href={item.action}>
-      <path
-        d={getPieData(radius, initialAngle, increment)}
-        fill="#E0E0E0"
-        stroke="#cecece"
-        strokeWidth="1" />
-      <image
-        style={{color: '#404040'}}
-        transform={`translate(-${ radius * 0.1 } -${ radius * 0.1 })`}
-        width={`${radius * 0.2}px`}
-        height={`${radius * 0.2}px`}
-        x={getX(centerX, (radius * 0.78), getAngleBetween(initialAngle, initialAngle + increment))}
-        y={getY(centerY, (radius * 0.78), getAngleBetween(initialAngle, initialAngle + increment))}
-        xlinkHref={item.image}
-      />
+    <path
+      d={getPieData(radius, initialAngle, increment)}
+      fill="#E0E0E0"
+      stroke="#cecece"
+      strokeWidth="1" />
+    <image
+      style={{color: '#404040'}}
+      transform={`translate(-${ radius * 0.1 } -${ radius * 0.1 })`}
+      width={`${radius * 0.2}px`}
+      height={`${radius * 0.2}px`}
+      x={getX(radius, (radius * 0.78), getAngleBetween(initialAngle, initialAngle + increment))}
+      y={getY(radius, (radius * 0.78), getAngleBetween(initialAngle, initialAngle + increment))}
+      xlinkHref={item.image}
+    />
   </a>
 )
 
 Item.propTypes = {
-  item: React.PropTypes.object,
+  item: React.PropTypes.shape({
+    action: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.func
+    ]),
+    image: React.PropTypes.string,
+    name: React.PropTypes.string
+  }),
   radius: React.PropTypes.number,
   initialAngle: React.PropTypes.number,
   increment: React.PropTypes.number,
-  centerX: React.PropTypes.number,
-  centerY: React.PropTypes.number,
+  actions: React.PropTypes.func
 }
 
 export default Item
